37628d3d0d160b889d33c1b0e569fd9fecc5d28c
slwaller <hockeywalt@yahoo.com>
2015-01-23 18:09:34 -0500

    boat controller

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -3,11 +3,22 @@ class BoatsController < ApplicationController
   def index
     @boat = Boat.all
   end
+
   def show
     @boat = Boat.find params[:id]
   end
+  
   def create
+    @boat = Boat.create boat_params
+    respond_to do |format|
+      if @boat.save
+        format.html { redirect_to @boat, notice: 'Boat was succesfully added!'}
+      else
+        format.html { render :new }
+      end
+    end
   end
+
   def new
     @boat = Boat.new
   end
